15fcdea61c726817d6dbb7ed7140707e02af75e2
EtoKazuki <34499576+EtoKazuki@users.noreply.github.com>
2019-05-07 20:46:54 +0900

    Update Interface.py

--- code_dir/Interface.py
+++ code_dir/Interface.py
@@ -10,12 +10,4 @@ q = input("素数を入力してください")
 # pri = Prime(p, q)
 el = ElGamal(q)
 
-'''
-print(pri.seek_L())
-print(pri.seek_E())
-print(pri.seek_D())
-print(pri.N)
-
-# print(pri.output())
-'''
 print(el.seek_root())
